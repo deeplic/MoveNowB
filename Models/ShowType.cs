f8020f36fe982508ceede3503cfe6c9a29f9c73b
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-21 14:49:02 +0200

    Models,create enum ShowType to solve propertry error in Car model

---
+++ Models/ShowType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Models
+{
+    public enum ShowType
+    {
+        Normal,
+        Advert
+    }
+}

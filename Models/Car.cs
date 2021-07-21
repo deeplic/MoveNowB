2768d595a288bdfefa69c4f8786587e3ce1ff787
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-21 14:45:16 +0200

    Models, create Car model with data annotations

---
+++ Models/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Models
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        [Required]
+        [Display(Name = "Brand Name")]
+        public string BrandName { get; set; }
+        [Required]
+        [Display(Name = "Model Name")]
+        public string ModelName { get; set; }
+        [Required]
+        public string Year { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        [Display(Name = "Show Type")]
+        public ShowType? ShowType { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}

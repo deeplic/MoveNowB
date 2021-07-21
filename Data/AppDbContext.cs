a74de3c553a5844efac19366a818a785d9dcad37
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-21 15:27:49 +0200

    Create Database with MSSQL, create  Data folder,add AppDbContext, add Nuget packege: EntityFrameworkCore

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoveNowB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<ShowCase> ShowCases { get; set; }
+    }
+}

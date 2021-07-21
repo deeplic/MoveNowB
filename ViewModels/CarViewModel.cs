2bdc6faf89ca5bed161a326b4cc9d62c39b52b0b
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-21 20:24:33 +0200

    Add ViewModels Folder

---
+++ ViewModels/CarViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using MoveNowB.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.ViewModels
+{
+    public class CarViewModel
+    {
+        public int CarID { get; set; }
+        [Required]
+        [Display(Name = "Brand Name")]
+        public string BrandName { get; set; }
+        [Required]
+        [Display(Name = "Model Name")]
+        public string ModelName { get; set; }
+        [Required]
+        public string Year { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        [Display(Name = "Show Type")]
+        public ShowType? ShowType { get; set; }
+        public IFormFile Photo { get; set; }
+        public string pageHeader { get; set; }
+    }
+}

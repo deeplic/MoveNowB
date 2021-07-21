d6c5ed14fba1558bf5e9936fb88c21b96e5515b4
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-21 14:53:47 +0200

    Models,create ShowCase Model

---
+++ Models/ShowCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Models
+{
+    public class ShowCase
+    {
+        [Key]
+        public int ShowID { get; set; }
+        [Display(Name = "Short Description")]
+        public string ShortDescription { get; set; }
+        [Display(Name = "Long Description")]
+        public string LongDescription { get; set; }
+        [Display(Name = "Brand Name")]
+        public string BrandName { get; set; }
+        [Display(Name = "Show Type")]
+        public ShowType? ShowType { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}

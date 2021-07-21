48141e03ca046a2a55985b8aed29bee3c3bfd168
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-21 19:48:32 +0200

    Services: Interfaces=>IShowCaseRepository,Repositories=>ShowCaseRepository

---
+++ Services/Interfaces/IShowCaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using MoveNowB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Services.Interfaces
+{
+    public interface IShowCaseRepository
+    {
+        ShowCase AddShow(ShowCase showCase);
+        ShowCase GetShowCaseById(int id);
+        IEnumerable<ShowCase> GetHideShowCases();
+        IEnumerable<ShowCase> GetShowCases();
+        ShowCase UpdateShow(ShowCase updatedShow);
+        ShowCase DeleteShow(int id);
+    }
+}
---
+++ Services/Repositories/ShowCaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using MoveNowB.Data;
+using MoveNowB.Models;
+using MoveNowB.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Services.Repositories
+{
+    public class ShowCaseRepository:IShowCaseRepository
+    {
+        private readonly AppDbContext _context;
+        public ShowCaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public ShowCase AddShow(ShowCase showCase)
+        {
+            _context.ShowCases.Add(showCase);
+            _context.SaveChanges();
+            return showCase;
+        }
+
+        public ShowCase DeleteShow(int id)
+        {
+            ShowCase showCase = _context.ShowCases.Find(id);
+            if (showCase != null)
+            {
+                _context.ShowCases.Remove(showCase);
+                _context.SaveChanges();
+            }
+            return showCase;
+        }
+
+        public IEnumerable<ShowCase> GetShowCases()
+        {
+            return _context.ShowCases.Where(x => x.ShowType == ShowType.Advert);
+        }
+        public IEnumerable<ShowCase> GetHideShowCases()
+        {
+            return _context.ShowCases;
+        }
+        public ShowCase GetShowCaseById(int id)
+        {
+            return _context.ShowCases.Find(id);
+        }
+
+        public ShowCase UpdateShow(ShowCase updatedShow)
+        {
+            var show = _context.ShowCases.Attach(updatedShow);
+            show.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return updatedShow;
+        }
+    }
+}

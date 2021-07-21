e2168161eef7ea0427c0d7c2b9e38567db775d64
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-21 19:32:42 +0200

    Services: Add Inferfaces=>ICarRepository and Repositories=>CarRepository,content complete

---
+++ Services/Interfaces/ICarRepository.cs
@@ -0,0 +1,20 @@
+﻿using MoveNowB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Services.Interfaces
+{
+    public interface ICarReposity
+    {
+        Car AddCar(Car car);
+        Car GetCarById(int id);
+        IEnumerable<Car> GetCarByBrand(string name);
+        IEnumerable<Car> GetAllCars();
+        IEnumerable<Car> GetHomeCars();
+        List<string> GetBrands();
+        Car UpdateCar(Car car);
+        Car DeleteCar(int id);
+    }
+}
---
+++ Services/Repositories/CarRepository.cs
@@ -0,0 +1,73 @@
+﻿using MoveNowB.Data;
+using MoveNowB.Models;
+using MoveNowB.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Services.Repositories
+{
+    public class CarRepository:ICarReposity
+    {
+        private readonly AppDbContext _context;
+        public CarRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Car AddCar(Car car)
+        {
+            _context.Cars.Add(car);
+            _context.SaveChanges();
+            return car;
+        }
+
+        public Car DeleteCar(int id)
+        {
+            Car car = _context.Cars.Find(id);
+            if (car != null)
+            {
+                _context.Cars.Remove(car);
+                _context.SaveChanges();
+            }
+            return car;
+        }
+
+        public IEnumerable<Car> GetAllCars()
+        {
+            var result = _context.Cars;
+            return result;
+        }
+
+        public List<string> GetBrands()
+        {
+            var results = _context.Cars.ToList().Select(x => x.BrandName).Distinct().ToList();
+            return results;
+        }
+
+        public IEnumerable<Car> GetCarByBrand(string name)
+        {
+            return _context.Cars.Where(x => x.BrandName == name);
+        }
+
+        public Car GetCarById(int id)
+        {
+            return _context.Cars.Find(id);
+        }
+
+        public IEnumerable<Car> GetHomeCars()
+        {
+            return _context.Cars.Where(x => x.ShowType == ShowType.Advert);
+        }
+
+        public Car UpdateCar(Car car)
+        {
+            if (car != null)
+            {
+                _context.Cars.Update(car);
+                _context.SaveChanges();
+            }
+            return car;
+        }
+    }
+}

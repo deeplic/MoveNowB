b12b5852f6c45b6b3573aac9647e66065396bab5
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-21 13:59:53 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoveNowB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

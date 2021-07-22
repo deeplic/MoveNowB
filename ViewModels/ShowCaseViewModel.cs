4f52a1f2080e676243dee00ea7b31f4ebca2d506
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-22 19:44:35 +0200

    Controllers=> add ShowCases controller; Views=> add ShowCases folder; ViewModels=>add ShowCaseViewModel

---
+++ ViewModels/ShowCaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using MoveNowB.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.ViewModels
+{
+    public class ShowCaseViewModel
+    {
+        [Display(Name = "Short Description")]
+        public string ShortDescription { get; set; }
+        [Display(Name = "Long Description")]
+        public string LongDescription { get; set; }
+        [Display(Name = "Brand Name")]
+        public string BrandName { get; set; }
+        [Display(Name = "Show Type")]
+        public ShowType? ShowType { get; set; }
+        public IFormFile Photo { get; set; }
+        public string pageHeader { get; set; }
+    }
+}

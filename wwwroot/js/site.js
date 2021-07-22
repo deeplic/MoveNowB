2a5796ae9e7076a7c5e7a799c970c57d079e7517
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-22 22:14:17 +0200

    Views/ShowCases=>Index view; wwwroot/js=>site.js=> initialize collapsible

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -15,3 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
         indicators: false
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
+});

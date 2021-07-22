51bfdcdba83d6c891c7ff02b11dfe8fc89cd31f5
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-22 12:40:35 +0200

    ViewModels=>add DetailViewModel; Controllers=>HomeController=>add Details action;

---
+++ ViewModels/DetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MoveNowB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.ViewModels
+{
+    public class DetailViewModel
+    {
+        public Car Car { get; set; }
+        public string pageHeader { get; set; }
+    }
+}

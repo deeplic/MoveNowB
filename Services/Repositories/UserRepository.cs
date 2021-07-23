b087198b04fac921dcb02fdf928b80676e4a7d61
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-23 23:00:48 +0200

    Services=>Interfaces=> add IUserRepository; Services=>Repositories=> add UserRepository;

---
+++ Services/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using MoveNowB.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Services.Repositories
+{
+    public class UserRepository:IUserRepository
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UserRepository(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string GetUserId()
+        {
+            return _httpContextAccessor.HttpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}

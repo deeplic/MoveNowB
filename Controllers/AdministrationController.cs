fa230d1638850ff9bfc48d7428666d10864a70d8
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-24 12:24:59 +0200

    ViewModels=> add CreateRoleViewModel; Controllers=> create AdministrationController=> add CreateRole httpget action; Views=> create Administration folder=>create CreateRole view; wwwroot/images=> add roles.svg;

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+    }
+}

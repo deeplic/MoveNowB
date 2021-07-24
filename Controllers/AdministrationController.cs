349d0fb5934f31247bc76d463c9141e0b0afdca1
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-24 13:20:03 +0200

    ViewModels=>create EditRoleViewModel; Controllers=>AdministrationController=>add Edit httppost action; Views/Administration=> add EditRole view;

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -55,5 +55,38 @@ namespace MoveNowB.Controllers
             var roles = _roleManager.Roles;
             return View(roles);
         }
+        // Role ID is passed from the URL to the action
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            // Find the role by Role ID
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            // Retrieve all the Users
+            foreach (var user in _userManager.Users.ToList())
+            {
+                // If the user is in this role, add the username to
+                // Users property of EditRoleViewModel. This model
+                // object is then passed to the view for display
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
     }
 }

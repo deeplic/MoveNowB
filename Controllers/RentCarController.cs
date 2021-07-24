2862fd10265d32209752d79ed62471dbe19e2329
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-24 10:55:36 +0200

    Controller=>add RentCarController add Index httpget action and NewRent action; Views/RentCar=>add Index view; Views/Home=>add routes to the views for NewRent;

---
+++ Controllers/RentCarController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MoveNowB.Models;
+using MoveNowB.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveNowB.Controllers
+{
+    public class RentCarController : Controller
+    {
+        private readonly IRentCarRepository _rentCarReposity;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserRepository _userService;
+        public RentCarController(IRentCarRepository rentCarReposity, UserManager<ApplicationUser> userManager, IUserRepository userService)
+        {
+            _rentCarReposity = rentCarReposity;
+            _userManager = userManager;
+            _userService = userService;
+        }
+        public IActionResult Index()
+        {
+            /*var userid = _userService.GetUserId();
+            string username = _userManager.Users.FirstOrDefault(x => x.Id == userid).UserName;
+            var model = _rentCarReposity.RentsByUser(username);*/
+            return View(/*model*/);
+        }
+        public IActionResult NewRent(int id)
+        {
+            _rentCarReposity.rentCar(id, "rent");
+            return RedirectToAction("Index");
+        }
+    }
+}

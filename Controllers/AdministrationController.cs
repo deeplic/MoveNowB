f6e0100ea24311acafd1eafa0fb8535130a295cd
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2021-07-24 12:34:28 +0200

    Controller=>AdministrationController=> do constructor injection and add CreateRole httppost action;

--- Controllers/AdministrationController.cs
+++ Controllers/AdministrationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MoveNowB.Models;
+using MoveNowB.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +11,44 @@ namespace MoveNowB.Controllers
 {
     public class AdministrationController : Controller
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
         [HttpGet]
         public IActionResult CreateRole()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // We just need to specify a unique role name to create a new role
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                // Saves the role in the underlying AspNetRoles table
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("listroles", "administration");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
         }
     }
 }
